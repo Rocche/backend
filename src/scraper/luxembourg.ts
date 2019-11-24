0db2fa55c92445beff3e59bda3a3bb2a8c0ce04d
Michele Biondi <michelebiondi01@gmail.com>
2019-11-24 15:05:11 +0100

    Add sample click not headless

--- src/scraper/luxembourg.ts
+++ src/scraper/luxembourg.ts
@@ -1,3 +1,20 @@
-export const isServiceAvailable = (): boolean => true;
+import { Builder, By } from 'selenium-webdriver';
 
-export const scrapeData = (): boolean => true;
+const startUrl =
+    'https://statistiques.public.lu/stat/TableViewer/tableView.aspx?ReportId=13050&IF_Language=eng&MainTheme=3&FldrName=5&RFPath=48';
+
+export const isServiceAvailable = async (): Promise<boolean> => {
+    return true;
+};
+
+export const scrapeData = async (): Promise<boolean> => {
+    const driver = await new Builder().forBrowser('firefox').build();
+    await driver.get(startUrl);
+    await driver.findElement(By.name('CSVButton')).click();
+    //await driver.findElement(By.name('q')).sendKeys('Selenium', Key.RETURN);
+    //await driver.quit();
+
+    return true;
+};
+
+isServiceAvailable();
