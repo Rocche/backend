cfcaa62f8380f410896923dd78f3876c27057bec
Michele Biondi <michelebiondi01@gmail.com>
2019-11-24 15:52:56 +0100

    Improved Firefox options for scraper

--- src/scraper/luxembourg.ts
+++ src/scraper/luxembourg.ts
@@ -1,18 +1,17 @@
 import { Builder, By } from 'selenium-webdriver';
 import * as firefox from 'selenium-webdriver/firefox';
 
+const startUrl =
+    'https://statistiques.public.lu/stat/TableViewer/tableView.aspx?ReportId=13050&IF_Language=eng&MainTheme=3&FldrName=5&RFPath=48';
+
+const downloadDir = '/home/an4cr0n/Downloads/Selenium';
+
 const options = new firefox.Options();
 
 options.headless();
-options.setPreference('browser.download.dir', '/home/an4cr0n/Downloads/Selenium');
+options.setPreference('browser.download.dir', downloadDir);
 options.setPreference('browser.download.folderList', 2);
-options.setPreference(
-    'browser.helperApps.neverAsk.saveToDisk',
-    'application/x-csv, text/plain, application/octet-stream, application/binary, text/csv, application/csv, application/excel, text/comma-separated-values, text/xml, application/xml',
-);
-
-const startUrl =
-    'https://statistiques.public.lu/stat/TableViewer/tableView.aspx?ReportId=13050&IF_Language=eng&MainTheme=3&FldrName=5&RFPath=48';
+options.setPreference('browser.helperApps.neverAsk.saveToDisk', 'application/x-csv');
 
 export const isServiceAvailable = async (): Promise<boolean> => {
     return true;
