e4a2172fcefc31244fe2ee922ff718144f7eb58b
Michele Biondi <michelebiondi01@gmail.com>
2019-11-24 16:26:55 +0100

    Add API to interact with scraper

--- src/routes/scraper/luxembourg.ts
+++ src/routes/scraper/luxembourg.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 
 import * as scraper from './../../scraper/luxembourg';
+
 const router = Router();
 
-router.get('/status', (req, res) => (scraper.isServiceAvailable() ? res.sendStatus(200) : res.sendStatus(502)));
+router.get('/status', async (req, res) =>
+    (await scraper.isServiceAvailable()) ? res.sendStatus(200) : res.sendStatus(502),
+);
 
-router.get('/data', (req, res) => (scraper.scrapeData() ? res.sendStatus(200) : res.sendStatus(502)));
+router.get('/download', async (req, res) => ((await scraper.scrapeData()) ? res.sendStatus(200) : res.sendStatus(502)));
 
 export default router;
