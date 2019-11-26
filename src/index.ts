e23fa9a95e9eead927a94a4af181dcdf382fd8c0
Michele Biondi <michelebiondi01@gmail.com>
2019-11-26 21:39:50 +0100

    Add helmet.js to increase security

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 
+import helmet from 'helmet';
+
 import luxembourg from './routes/scraper/luxembourg';
 
 import { getData } from './loader'; // getData will return luxembourg's data so far
+
 const app = express();
+app.use(helmet()); // Add security headers
 
 app.get('/', (request, response) => {
     response.send('Hello world!');
