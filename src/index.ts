043ebecc95ea1a52a41e8dd84d188a746c439da6
Michele Biondi <michelebiondi01@gmail.com>
2019-11-26 20:35:30 +0100

    Add swagger route to backend

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,17 @@ import express from 'express';
 
 import luxembourg from './routes/scraper/luxembourg';
 
+import * as swaggerUi from 'swagger-ui-express';
+
+import * as YAML from 'yamljs';
+
 import { getData } from './loader'; // getData will return luxembourg's data so far
+
+const swaggerDocument = YAML.load('swagger.yaml');
+
 const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (request, response) => {
     response.send('Hello world!');
